5cbcf29f6d1bbd5393eb9bd15870c85c74ab9dc4
deo <deotri4@gmail.com>
2025-12-16 22:51:09 +0800

    logout function

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -80,4 +80,15 @@ const login = async (req, res) => {
   });
 };
 
-export { register, login };
+const logout = (req, res) => {
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({
+    status: 'success',
+    message: 'Logged out successfully',
+  });
+};
+
+export { register, login, logout };
