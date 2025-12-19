0fd62b483da913b64bdf34afc539edf5c025e20e
deo <deotri4@gmail.com>
2025-12-19 23:31:51 +0800

    feat: middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,4 @@
+import jwt from 'jsonwebtoken';
+import { prisma } from '../config/db.js';
+
+export const authMiddleware = async (req, res, next) => {};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { register, login, logout } from '../controllers/authControllers.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
+
+router.use(authMiddleware);
 
 router.post('/register', register);
 router.post('/login', login);
