2b86b0ed95694176bff5e8ebb36b949c101f2d68
deo <deotri4@gmail.com>
2025-12-20 15:37:12 +0800

    feat: validation using ZOD

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -5,11 +5,17 @@ import {
   removeFromWatchlist,
 } from '../controllers/watchlistController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
+import { validateRequest } from '../middleware/validateRequest.js';
+import { addToWatchlistSchema } from '../validators/watchlistValidators.js';
 
 const router = express.Router();
 router.use(authMiddleware);
 
-router.post('/watchlist', addToWatchlist);
+router.post(
+  '/watchlist',
+  validateRequest(addToWatchlistSchema),
+  addToWatchlist
+);
 
 // {{BaseURL}}/api/v1/movies/watchlist/:id
 router.put('/watchlist/:id', updateWatchlistItem);
